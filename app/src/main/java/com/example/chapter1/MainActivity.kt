3f5f442f68e97634399637e250bcba20768ca9c2
suyeong2785 <qkdtpwls_@naver.com>
2024-01-05 01:06:36 +0900

    24/01/05 방수영 1. Fragment를 두개의 버튼을 통해서 Activity안의 FrameLayout에 교체하는 기능구현

--- app/src/main/java/com/example/chapter1/MainActivity.kt
+++ app/src/main/java/com/example/chapter1/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.FragmentManager
+import com.example.chapter1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val container = binding.fragmentContainer
+
+        binding.button1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(container.id, WebViewFragment())
+                    .commit()
+            }
+        }
+
+        binding.button2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(container.id, BFragment())
+                    .commit()
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/chapter1/WebViewFragment.kt
@@ -0,0 +1,36 @@
+package com.example.chapter1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.example.chapter1.databinding.FragmentWebviewBinding
+
+
+class WebViewFragment : Fragment() {
+    private lateinit var binding: FragmentWebviewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentWebviewBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+
+        binding.webView.loadUrl("https://google.com")
+
+
+    }
+}
